13273d0d29a854da7e27740746d9dabcb3932bb5
Wyatt LeFevre <wyatt@neighbor.com>
2022-09-28 10:39:51 -0600

    latest

--- keyboards/sofle/keymaps/wylord/keymap.c
+++ keyboards/sofle/keymaps/wylord/keymap.c
@@ -21,37 +21,50 @@
 
 //Default keymap. This can be changed in Via. Use oled.c and encoder.c to change beavior that Via cannot change.
 
+enum {
+  BASE = 0,
+  NUM = 1,
+  BRACK = 2,
+  ARROWS = 3,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MAC Layout
-[0] = LAYOUT(
-  LT(1,KC_GRV),KC_1,   KC_2,   KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  LCTL_T(KC_ESC),KC_A,KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-               KC_LGUI,KC_LALT,KC_LCTRL, KC_SPC, MO(3),            KC_ENT, KC_SPC, KC_RCTRL, KC_RALT, KC_RGUI
+[BASE] = LAYOUT(
+  LT(NUM,KC_GRV),   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MUTE,                 _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+               LCTL(KC_LALT),KC_LALT,KC_LGUI, KC_SPC, MO(ARROWS),          KC_ENT, KC_SPC, RCTL(KC_BSLS), _______, _______ 
+),
+[NUM] = LAYOUT(
+  _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                    _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
+  _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,                    _______,KC_7,   KC_8,   KC_9,   KC_PPLS,  KC_BSPC,
+  _______, KC_F11, KC_F12,_______,_______,_______,                    _______,KC_4,   KC_5,   KC_6,   KC_PPLS, _______,
+  _______,_______,_______,_______,_______,_______,_______,    _______,_______,KC_1,   KC_2,   KC_3,   KC_PENT, _______,
+                  _______,_______,_______,_______,_______,    _______,_______,KC_0,   KC_0,   KC_DOT
 ),
 
-[1] = LAYOUT(
-  XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_7,    KC_8,    KC_9, KC_SCLN,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_4,    KC_5,    KC_6,    KC_O,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,   KC_1,    KC_2,    KC_3, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTRL,KC_TRNS, KC_ENT,        KC_SPC,  KC_TRNS,  KC_0, KC_RALT, KC_RGUI
+[BRACK] = LAYOUT(
+  _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LPRN,KC_RPRN,_______,_______,
+  _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LCBR,KC_RCBR,_______,_______,
+  _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LBRC,KC_RBRC,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
+              _______,_______,_______,_______,_______,            _______,_______,_______,_______,_______
 ),
 
-[2] = LAYOUT(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-                       _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+[ARROWS] = LAYOUT(
+  _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,                    KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
+              _______,_______,_______,_______,_______,            _______,KC_RCTL,_______,_______,_______
 ),
-
-[3] = LAYOUT(
-  _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
-  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, _______,   KC_UP, _______,_______, KC_BSPC,
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, _______, XXXXXXX, _______,   XXXXXXX, _______,
-                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
-)
+// [4] = LAYOUT(
+//   _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
+//   _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
+//   _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
+//   _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
+//               _______,_______,_______,_______,_______,            _______,_______,_______,_______,_______
+// ),
 };
