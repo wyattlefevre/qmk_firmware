1073b7a6884fe6bd11af99d5ba9ed6cfeec86b84
Wyatt LeFevre <wyatt@neighbor.com>
2022-10-05 11:09:38 -0600

    animations!

--- keyboards/sofle/keymaps/wylord/keymap.c
+++ keyboards/sofle/keymaps/wylord/keymap.c
@@ -16,10 +16,21 @@
   */
 
 #include QMK_KEYBOARD_H
-#include "oled.c"
+
+#include <stdio.h>
+
+#define ANIM_INVERT false
+#define ANIM_RENDER_WPM true
+#define FAST_TYPE_WPM 45 //Switch to fast animation when over words per minute
+
+#ifdef OLED_DRIVER_ENABLE
+    #include "crab.c"
+#endif
+// char wpm_str[10];
 #include "encoder.c"
 
 //Default keymap. This can be changed in Via. Use oled.c and encoder.c to change beavior that Via cannot change.
+
 
 enum {
   BASE = 0,
@@ -31,34 +42,34 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MAC Layout
 [BASE] = LAYOUT(
-  LT(NUM,KC_GRV),   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  LT(NUM,KC_1),     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                            KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
   LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MUTE,                 _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-               LCTL(KC_LALT),KC_LALT,KC_LGUI, KC_SPC, MO(ARROWS),          KC_ENT, KC_SPC, RCTL(KC_BSLS), _______, _______ 
+  KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MUTE,                 LCTL(LGUI(KC_Q)),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+        LCTL(KC_LALT),KC_LALT,MO(ARROWS),KC_SPC, KC_LGUI,                KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS),  _______ 
 ),
 [NUM] = LAYOUT(
   _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                    _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
   _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,                    _______,KC_7,   KC_8,   KC_9,   KC_PPLS,  KC_BSPC,
-  _______, KC_F11, KC_F12,_______,_______,_______,                    _______,KC_4,   KC_5,   KC_6,   KC_PPLS, _______,
+  _______, _______, _______,_______,KC_F11,KC_F12,                    _______,KC_4,   KC_5,   KC_6,   KC_PPLS, _______,
   _______,_______,_______,_______,_______,_______,_______,    _______,_______,KC_1,   KC_2,   KC_3,   KC_PENT, _______,
                   _______,_______,_______,_______,_______,    _______,_______,KC_0,   KC_0,   KC_DOT
 ),
 
 [BRACK] = LAYOUT(
+  _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LPRN,KC_RPRN,_______,_______,
-  _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LCBR,KC_RCBR,_______,_______,
   _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LBRC,KC_RBRC,_______,_______,
-  _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
-              _______,_______,_______,_______,_______,            _______,_______,_______,_______,_______
+  _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,KC_LCBR,KC_RCBR,_______,_______,
+              _______,_______,_______,_______,_______,           _______,_______,_______,_______,_______
 ),
 
 [ARROWS] = LAYOUT(
-  _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,KC_MPRV,KC_MNXT,
   _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,                    KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,_______,_______,
   _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
-              _______,_______,_______,_______,_______,            _______,KC_RCTL,_______,_______,_______
+              _______,_______,_______,_______,_______,            KC_RCTL,XXXXXXX,XXXXXXX,_______,_______
 ),
 // [4] = LAYOUT(
 //   _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
@@ -68,3 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //               _______,_______,_______,_______,_______,            _______,_______,_______,_______,_______
 // ),
 };
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+  oled_render_anim();
+}
+#endif
+
