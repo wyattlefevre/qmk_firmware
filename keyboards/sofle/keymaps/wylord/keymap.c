c8e7e0bd85921203a4b751fdfc7dcc953e5787e0
Wyatt LeFevre <wyatt@neighbor.com>
2022-10-08 14:07:48 -0600

    latest changes

--- keyboards/sofle/keymaps/wylord/keymap.c
+++ keyboards/sofle/keymaps/wylord/keymap.c
@@ -50,15 +50,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(NUM,KC_1),     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                            KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
   LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MUTE,                 LCTL(LGUI(KC_Q)),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-        LCTL(KC_LALT),KC_LALT,MO(ARROWS),KC_SPC, KC_LGUI,                KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS),  DF(GAME) 
+  KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MUTE,                LGUI(KC_L),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+        KC_LGUI, KC_LALT,MO(ARROWS),KC_SPC, KC_LCTL,                             KC_ENT, KC_SPC, KC_EQL, KC_BSLS,  DF(GAME) 
 ),
 [GAME] = LAYOUT(
-  LT(NUM,KC_1),     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                            KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
-  LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MUTE,                 LCTL(LGUI(KC_Q)),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-        LCTL(KC_LALT),KC_LALT,MO(ARROWS),KC_SPC, KC_LGUI,                KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS),  DF(MAC) 
+  XXXXXXX,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                              KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
+  KC_TAB,    KC_T,  KC_Q,  KC_W,  KC_E,  KC_R,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  KC_LCTL,   KC_G,  KC_A,  KC_S,  KC_D,  KC_F,                              KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LSFT,   KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_MUTE,   LCTL(LGUI(KC_Q)),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+        KC_LGUI, KC_LALT, KC_B,KC_SPC, KC_G,                  KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS),  DF(MAC) 
 ),
 [NUM] = LAYOUT(
   _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                    _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BRACK] = LAYOUT(
   _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
-  _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LPRN,KC_RPRN,_______,_______,
+  _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LCBR,KC_RCBR,_______,_______,
   _______,_______,_______,_______,_______,_______,                    _______,_______,KC_LBRC,KC_RBRC,_______,_______,
-  _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,KC_LCBR,KC_RCBR,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
               _______,_______,_______,_______,_______,           _______,_______,_______,_______,_______
 ),
 
