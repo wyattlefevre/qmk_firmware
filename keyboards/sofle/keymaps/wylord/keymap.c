58289529ae66242fc3a921158f48ab067c5890cb
Wyatt LeFevre <wyatt@neighbor.com>
2022-10-21 09:45:28 -0600

    latest

--- keyboards/sofle/keymaps/wylord/keymap.c
+++ keyboards/sofle/keymaps/wylord/keymap.c
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC] = LAYOUT(
   LT(NUM,KC_1),     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                            KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
   LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  LT(NUM,KC_G),                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MPLY,                 LCTL(LGUI(KC_Q)),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
         LCTL(KC_LALT),KC_LALT,MO(ARROWS),LT(BRACK,KC_SPC), KC_LGUI,                KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS), _______ 
 ),
 [WINDOWS] = LAYOUT(
   LT(NUM,KC_1),     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                            KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
   LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  LT(NUM,KC_G),                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MPLY,                LGUI(KC_L),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
         KC_LGUI, KC_LALT,MO(ARROWS),LT(BRACK,KC_SPC), KC_LCTL,                             KC_ENT, KC_SPC, KC_EQL, KC_BSLS, _______
 ),
@@ -61,11 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LGUI, MO(ARROWS), KC_N,LT(BRACK,KC_SPC), KC_M,                  KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS), DF(MAC) 
 ),
 [NUM] = LAYOUT(
-  _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                    _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
-  _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,                    _______,KC_7,   KC_8,   KC_9,   KC_PPLS,  KC_BSPC,
-  _______, _______, _______,_______,KC_F11,KC_F12,                    _______,KC_4,   KC_5,   KC_6,   KC_PPLS, _______,
-  _______,_______,_______,_______,_______,_______,_______,    _______,_______,KC_1,   KC_2,   KC_3,   KC_PENT, _______,
-                  _______,_______,_______,_______,_______,    _______,_______,KC_0,   KC_0,   KC_DOT
+  _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                    _______,KC_7,   KC_8,   KC_9,   KC_PMNS, _______,
+  _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,                    _______,KC_4,   KC_5,   KC_6,   KC_PPLS,  KC_BSPC,
+  _______, _______, _______,_______,KC_F11,KC_F12,                    _______,KC_1,   KC_2,   KC_3,   KC_PPLS, _______,
+  _______,_______,_______,_______,_______,_______,_______,    _______,_______,KC_0,   KC_0,   KC_DOT,   KC_PENT, _______,
+                  _______,_______,_______,_______,_______,    _______,_______,_______,_______,_______   
 ),
 
 [BRACK] = LAYOUT(
