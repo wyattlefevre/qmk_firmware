59ef329de4063110642a7430753f662724c1eb6a
Wyatt LeFevre <wyatt@Wyatts-MacBook-Pro.local>
2022-12-17 06:22:22 -0700

    mission control button

--- keyboards/sofle/keymaps/wylord/keymap.c
+++ keyboards/sofle/keymaps/wylord/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(BRACK,KC_TAB), KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   LCTL_T(KC_ESC),   KC_A,  KC_S,  KC_D,  KC_F,  LT(NUM,KC_G),                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_MPLY,                 LCTL(LGUI(KC_Q)),KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-        LCTL(KC_LALT),KC_LALT,MO(ARROWS),LT(BRACK,KC_SPC), KC_LGUI,                KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS), _______ 
+        LCTL(KC_LALT),KC_LALT,MO(ARROWS),LT(BRACK,KC_SPC), KC_LGUI,                KC_ENT, KC_SPC, KC_EQL, RCTL_T(KC_BSLS), RCTL(KC_UP) 
 ),
 [WINDOWS] = LAYOUT(
   LT(NUM,KC_1),     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                            KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_GRV,
